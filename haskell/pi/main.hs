87d7429bd2726ed1bd4d85a741578283fdf79fb1
Ard-Jan van Etten <ardjanve@gmail.com>
2020-09-28 13:56:18 +0200

    n -> r

--- haskell/pi/main.hs
+++ haskell/pi/main.hs
@@ -2,15 +2,15 @@ incirkle :: [Int] -> Int -> Bool
 incirkle point max = sum (map (^2) point) < max ^ 2
 
 line :: Int -> Int -> Int
-line n q = length $ filter (\p -> incirkle [p, q] n) [0..n]
+line r q = length $ filter (\p -> incirkle [p, q] r) [0..r]
 
 inbox :: Int -> Int
-inbox n = sum [line n q | q <- [0..n]]
+inbox r = sum [line r q | q <- [0..r]]
 
 ifdiv :: Int -> Int -> Float
 ifdiv a b = (fromIntegral a) / (fromIntegral b)
 
 boxpi :: Int -> Float
-boxpi n = (inbox n * 4) `ifdiv` (n ^ 2)
+boxpi r = (inbox r * 4) `ifdiv` (r ^ 2)
 
 main = print $ boxpi 1000
