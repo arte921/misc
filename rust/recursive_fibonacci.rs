3dbd7924448cadc503bb435cb752a0f6d5623ec9
Ard-Jan van Etten <ardjanve@gmail.com>
2020-10-01 08:07:04 +0200

    bruh

---
+++ rust/recursive_fibonacci.rs
@@ -0,0 +1,11 @@
+fn main () {
+    fibonacci(30, 1, 0);
+}
+
+fn fibonacci (i: i32, f: i32, v: i32) -> i32 {
+    if i == 0 {
+        return f;
+    }
+    println!("{}", f);
+    return fibonacci(i - 1, f + v, f);
+}
