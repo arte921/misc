1833dc22c51773a5bc615490153a67add1a096d1
Ard-Jan van Etten <ardjanve@gmail.com>
2021-06-19 21:10:59 +0200

    ee

--- js/fibonacci-oneliner.js
+++ js/fibonacci-oneliner.js
@@ -1,8 +1,1 @@
-/*[...Array(100).keys()]
-    .map((lengte) =>
-        [...Array(lengte + 2).keys()]
-            .reduce((totaal) => totaal ? [totaal[0] + totaal[1], totaal[0]] : [0, 1])[0]
-    )
-    .forEach((waarde) => console.log(waarde));*/
-
-    [...Array(10).keys()].reduce((totaal, _a, _b, _c, waarde = (totaal ? [totaal[0] + totaal[1], totaal[0]] : [0, 1]), _d = console.log(waarde[0])) => waarde);
+[...Array(10).keys()].reduce((totaal, _a, _b, _c, waarde = (totaal ? [totaal[0] + totaal[1], totaal[0]] : [0, 1]), _d = console.log(waarde[0])) => waarde);
